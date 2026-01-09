74b1a720020f49b63fa0f9e765ff4e0cd8ef3d67
sudayseali <sudaysealiumar@gmail.com>
2026-01-09 19:38:36 +0300

    Initial React Native app with APK build via GitHub Actions

---
+++ App.js
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+      <Text>App-ku si sax ah ayuu u shaqeynayaa 🚀</Text>
+    </View>
+  );
+}
